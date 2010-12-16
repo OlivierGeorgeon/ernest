a591df7e6e264a06a959ddf6312218bcb9dff61a
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@55d8f869-0c07-9a38-3be9-08337c01e676>
2010-12-16 19:08:50 +0000

    Preliminary implementation of sensory schemas. This version is bugged.

--- e-ernest/src/ernest/IEnvironment.java
+++ e-ernest/src/ernest/IEnvironment.java
@@ -1,7 +1,5 @@
 package ernest;
 
-import java.util.List;
-
 /**
  * Interface for an Environment suitable to Ernest.
  * @author mcohen
@@ -9,5 +7,10 @@ import java.util.List;
  */
 public interface IEnvironment 
 {
+	/**
+	 * Enact a primitive schema and return the enaction status.
+	 * @param s The primitive schema that Ernest has chosen to enact.
+	 * @return The status that results from the enaction of the primitive schema in the environment.
+	 */
 	public boolean enact(String s);
 }
