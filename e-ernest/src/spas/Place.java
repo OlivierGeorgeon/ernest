84584529cd620f2e921e641206f758a3da98dad2
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@55d8f869-0c07-9a38-3be9-08337c01e676>
2011-12-08 15:56:44 +0000

    Problem with svn saving Place.java

--- e-ernest/src/spas/Place.java
+++ e-ernest/src/spas/Place.java
@@ -3,8 +3,10 @@ package spas;
 import javax.vecmath.Matrix3f;
 import javax.vecmath.Vector3f;
 
+import ernest.Ernest;
+
 /**
- * A place is a location in the local space that holds a placeable item (stimulation, salience, or bundle).
+ * A place is a location in the local space that holds a bundle.
  * @author Olivier
  */
 public class Place implements IPlace 
@@ -12,10 +14,25 @@ public class Place implements IPlace
 
 	IBundle m_bundle;
 	Vector3f m_position;
+	float m_span;
+	int m_attractiveness;
 	
 	/**
-	 * @param bundle The location's bundle.
-	 * @param position The location's position.
+	 * @param bundle This place's bundle.
+	 * @param distance This place's distance.
+	 * @param direction This place's direction. 
+	 * @param span The span of this bundle at this place.
+	 */
+	public Place(IBundle bundle, float distance, float direction, float span)
+	{
+		m_bundle = bundle;
+		m_position = new Vector3f((float)(distance * Math.cos((double)direction)), (float)(distance * Math.sin((double)direction)), 0f);
+		m_span = span;
+	}
+	
+	/**
+	 * @param bundle The bundle at this place.
+	 * @param position This place's position.
 	 */
 	public Place(IBundle bundle, Vector3f position)
 	{
@@ -100,6 +117,60 @@ public class Place implements IPlace
 	private float polarAngle(Vector3f v) 
 	{
 		return (float)Math.atan2((double)v.y, (double)v.x);
+	}
+
+	public float getDirection() 
+	{
+		return (float)Math.atan2((double)m_position.y, (double)m_position.x);
+	}
+
+	public float getDistance() 
+	{
+		return m_position.length();
+	}
+	public float getSpan() 
+	{
+		return m_span;
+	}
+
+	public void setSpan(float span) 
+	{
+		m_span = span;
+	}
+	
+	public int getAttractiveness(int clock) 
+	{
+		// If it is in the background (visual place).
+		if (getDistance() > LocalSpaceMemory.DISTANCE_VISUAL_BACKGROUND - 1)
+			return (m_bundle.getExtrapersonalAttractiveness(clock) + (int)(5 * m_span / ((float)Math.PI / 12)));
+		
+		// Attractiveness of wall in front
+		else if (isFrontal() && m_bundle.getTactileValue() == Ernest.STIMULATION_TOUCH_WALL)
+			return m_bundle.getPeripersonalAttractiveness(clock);
+		
+		// Attractiveness of fish 
+		else if (m_bundle.getTactileValue() == Ernest.STIMULATION_TOUCH_FISH 
+				|| m_bundle.getTactileValue() == Ernest.STIMULATION_TOUCH_AGENT)// != Ernest.STIMULATION_TOUCH_EMPTY.getValue())
+			return m_bundle.getPeripersonalAttractiveness(clock);
+
+		return 0;
+	}
+	
+	public boolean isFrontal()
+	{
+		// Covers at least a pixel to the right and a pixel to the left of Ernest's axis.
+		return (getDirection() - m_span / 2 < - Math.PI/12 + 0.1 && getDirection() + m_span / 2 > Math.PI/12 - 0.1 );
+	}
+
+	public void setDistance(float distance)
+	{
+		m_position.normalize();
+		m_position.scale(distance);
+	}
+
+	public void setPosition(Vector3f position) 
+	{
+		m_position = position;
 	}
 
 }
