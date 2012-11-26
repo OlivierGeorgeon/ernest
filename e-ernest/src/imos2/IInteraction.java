2d2a1d1211a78bee0a5636097846d89ba3d47d86
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@55d8f869-0c07-9a38-3be9-08337c01e676>
2012-11-26 15:23:01 +0000

    Imos2 is under development.

--- e-ernest/src/imos2/IInteraction.java
+++ e-ernest/src/imos2/IInteraction.java
@@ -19,4 +19,19 @@ public interface IInteraction
 	public void setfailPostWeight(int failPostWeight);
 	public int getfailPostWeight();
 	public int getLength();
+	public void setStep(int step);
+	public void setPrescriber(IInteraction prescriber);
+	public IInteraction getPrescriber();
+	public int getStep();
+
+	/**
+	 * Update the prescriber if this interaction was enacted
+	 * @return The next top level interaction to enact or null if the current enaction is over.
+	 */
+	public IInteraction updatePrescriber();
+	
+	/**
+	 * Clear the prescriber hierarchy
+	 */
+	public void terminate();
 }
