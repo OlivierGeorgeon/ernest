eac656d458d6c0848e9c995d1cd3a12a1355d435
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@55d8f869-0c07-9a38-3be9-08337c01e676>
2013-07-04 15:35:02 +0000

    ActionSpasWeightComparator was removed

--- e-ernest/src/eca/decider/ActionPropositionComparator.java
+++ e-ernest/src/eca/decider/ActionPropositionComparator.java
@@ -6,14 +6,14 @@ import java.util.Comparator;
  * A comparator to sort Action Propositions by their descending SS weight
  * @author Olivier
  */
-public class ActionSSWeightComparator implements Comparator<ActionProposition>{
+public class ActionPropositionComparator implements Comparator<ActionProposition>{
 	
 	public static int SS = 0;
 	public static int SPAS = 1;
 	
 	private int system = 0;
 
-	ActionSSWeightComparator(int system){
+	ActionPropositionComparator(int system){
 		this.system = system;
 	}
 	
