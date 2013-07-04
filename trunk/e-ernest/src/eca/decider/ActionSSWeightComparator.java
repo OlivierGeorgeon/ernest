293b929e987491f87d6e444bf15ffc362c8b006f
olivier.georgeon@gmail.com <olivier.georgeon@gmail.com@55d8f869-0c07-9a38-3be9-08337c01e676>
2013-07-04 15:31:36 +0000

    Parametrized the action comparators.

--- trunk/e-ernest/src/eca/decider/ActionSSWeightComparator.java
+++ trunk/e-ernest/src/eca/decider/ActionSSWeightComparator.java
@@ -7,9 +7,20 @@ import java.util.Comparator;
  * @author Olivier
  */
 public class ActionSSWeightComparator implements Comparator<ActionProposition>{
+	
+	public static int SS = 0;
+	public static int SPAS = 1;
+	
+	private int system = 0;
 
+	ActionSSWeightComparator(int system){
+		this.system = system;
+	}
+	
     public int compare(ActionProposition p1, ActionProposition p2) {
-        return - Integer.valueOf(p1.getSSWeight()).compareTo(p2.getSSWeight());
+    	if (this.system == SS)
+    		return - Integer.valueOf(p1.getSSWeight()).compareTo(p2.getSSWeight());
+    	else
+            return - Integer.valueOf(p1.getSpasWeight()).compareTo(p2.getSpasWeight());
     }
-
 }
